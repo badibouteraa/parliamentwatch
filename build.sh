d19574f14d64b302d18ecabed5ea59ffcb0db169
Boris Hekele <hekele@parliamentwatch.org>
2012-05-23 09:56:47 +0200

    added custom modules

--- build.sh
+++ build.sh
@@ -21,10 +21,12 @@ else
 fi
 
 echo "RUNNING DRUSH MAKE FILE"
-drush make --yes --working-copy --contrib-destination=sites/all/modules/contrib parliamentwatch.make
+drush make --yes --working-copy parliamentwatch.make
 
 echo "INSTALLING SITE"
+mv sites custom_sites
 drush site-install standard --yes --locale=de --account-name=root --account-pass=$SITE_PASS --account-mail=dummy@parliamentwatch.org --site-name=parliamentwatch.org --db-url=mysql://root:$DB_PASS@localhost/$DB_NAME
+mv custom sites
 
 echo "DISABLING USELESS MODULES"
 drush dis --yes overlay toolbar
@@ -32,16 +34,10 @@ drush dis --yes overlay toolbar
 echo "ENABLING CONTRIBUTED MODULES"
 drush en --yes entity views views_ui admin_menu admin_menu_toolbar og og_access og_field_access og_ui og_register module_filter token pathauto migrate migrate_ui date date_api date_popup features masquerade taxonomy_manager relation* field_validation wordpress_migrate
 
-echo "CLONING CUSTOM MODULES FROM GIT"
-git clone git@github.com:parliamentwatch/custom_modules sites/all/modules/custom
-
 echo "ENABLING CUSTOM MODULES"
 drush en --yes constituency committee_type blogpost_type migrate_constituency migrate_committee migrate_party migrate_user
 
-echo "CLONING CUSTOM THEMES FROM GIT"
-git clone git@github.com:parliamentwatch/custom_themes sites/all/themes
-
-echo "ENABLING THEME"
+echo "ENABLING THEMES"
 drush en --yes omega abgeordnetenwatch
 
 echo "DEFAULT SETTINGS"
@@ -51,5 +47,5 @@ drush vset --yes site_default_country "DE"
 drush vset --yes theme_default abgeordnetenwatch
 
 # set access rights
-echo "SETTING PERMISSION TO root:www-data"
+echo "SETTING FILE SYSTEM PERMISSIONS TO root:www-data"
 chown -R root:www-data *
