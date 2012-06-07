653706adf4dacc300f6fa946c9ef6312b176c588
Boris Hekele <hekele@parliamentwatch.org>
2012-06-07 16:18:21 -0500

    AW-146
    new breadcrumb
    
    Signed-off-by: ruthverena75 <ruth.geisenfelder@gmail.com>

--- sites/all/themes/custom/abgeordnetenwatch/templates/ds-1col--node-blogpost.tpl.php
+++ sites/all/themes/custom/abgeordnetenwatch/templates/ds-1col--node-blogpost.tpl.php
@@ -1,40 +1,23 @@
-<div<?php print $attributes; ?>>
-  <?php print $user_picture; ?>
-  
-  <?php if (!$page && $title): ?>
-  <div>
-    <?php print render($title_prefix); ?>
-    <h2<?php print $title_attributes; ?>><a href="<?php print $node_url ?>" title="<?php print $title ?>"><?php print $title ?></a></h2>
-    <?php print render($title_suffix); ?>
-  </div>
-  <?php endif; ?>
-  
-  <?php if ($display_submitted): ?>
-  <div class="submitted"><?php print $date; ?> -- <?php print $name; ?></div>
-  <?php endif; ?>  
-  
-  <div<?php print $content_attributes; ?>>
+<?php
 
-    <?php
-      // We hide the comments and links now so that we can render them later.
-      hide($content['comments']);
-      hide($content['links']);
-	if(!function_exists('floatbox')): // If no floatbox then print normal contents
-	  print render($content); // render normal Contents
+/**
+ * @file
+ * Display Suite 1 column template.
+ */
+?>
+<div class="ds-1col <?php print $classes;?> clearfix <?php print $ds_content_classes;?>">
+
+  <?php if (isset($title_suffix['contextual_links'])): ?>
+  <?php print render($title_suffix['contextual_links']); ?>
+  <?php endif; ?>
+
+  <?php
+  	if(!function_exists('floatbox')): // If no floatbox then print normal contents
+	  print $ds_content; // render normal Contents
 	endif;
 
 	if(function_exists('floatbox')):  // If floatbox then print floatbox with contents
-	  print floatbox($content); // render Floatbox and normal contents merged
-	endif; 
-
-    ?>
-  </div>
-  
-  <div class="clearfix">
-    <?php if (!empty($content['links'])): ?>
-      <div class="links node-links clearfix"><?php print render($content['links']); ?></div>
-    <?php endif; ?>
-
-    <?php print render($content['comments']); ?>
-  </div>
+	  print floatbox($ds_content); // render Floatbox and normal contents merged
+	endif;
+   ?>
 </div>
