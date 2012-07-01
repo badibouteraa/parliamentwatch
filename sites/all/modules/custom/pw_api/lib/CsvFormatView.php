2396aa003748d24c50203baca48a3a104b60518c
Niklas Fiekas <niklas.fiekas@googlemail.com>
2012-07-01 16:01:59 +0200

    PW API: Improve CSV for one-entity-results.

--- sites/all/modules/custom/pw_api/lib/CsvFormatView.php
+++ sites/all/modules/custom/pw_api/lib/CsvFormatView.php
@@ -13,10 +13,26 @@ class CsvFormatView extends RESTServerView {
   public function render() {
     $buffer = fopen('php://temp', 'r+');
 
-    foreach ($this->model as $row) {
-      fputcsv($buffer, (array)$row);
+    // Decide if the resultset is a numerically keyed set of entities or
+    // a single entity and prepare the header row accordingly.
+    $keys = array_keys($this->model);
+    if (!is_numeric(reset($keys))) {
+      $rows = array($this->model);
+    }
+    else {
+      $keys = array_keys(reset($this->model));
+      $rows = $this->model;
     }
 
+    // Write the header row to the buffer.
+    fputcsv($buffer, $keys);
+
+    // Write the resultset to the buffer.
+    foreach ($rows as $row) {
+      fputcsv($buffer, $row);
+    }
+
+    // Read the buffer into a string.
     rewind($buffer);
     $csv = stream_get_contents($buffer);
     fclose($buffer);
