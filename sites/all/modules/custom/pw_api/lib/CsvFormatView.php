b2ee0594ec3b568b78fc591ceff7b0401e773fd2
Niklas Fiekas <niklas.fiekas@googlemail.com>
2012-07-01 15:02:43 +0200

    PW API: Reorganize file structure.

--- sites/all/modules/custom/pw_api/lib/CsvFormatView.php
+++ sites/all/modules/custom/pw_api/lib/CsvFormatView.php
@@ -12,12 +12,15 @@ class CsvFormatView extends RESTServerView {
 
   public function render() {
     $buffer = fopen('php://temp', 'r+');
+
     foreach ($this->model as $row) {
       fputcsv($buffer, (array)$row);
     }
+
     rewind($buffer);
     $csv = stream_get_contents($buffer);
     fclose($buffer);
+
     return $csv;
   }
 }
